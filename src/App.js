a6e49842c6e96f20e22e983be749ed6026fedfc1
guruprasath78 <guruprasathsmart@gmail.com>
2022-12-12 15:23:05 +0530

    2 commit

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route,redirect } from "react-router-dom";
 import ResponsiveAppBar from './project/NavBar';
 import AboutUs from './components/AboutUs';
 import Contact from './components/Contact';
 import Home from './project/Home';
 import FormEx from './form/FormEx';
+import GetEx from './Http/GetEx';
+import Tracker from './project/Tracker';
+import Rough from './project/rough';
 function App() {
   return (
     <div className='App'>
-    <FormEx/>
+    
     <BrowserRouter>
     <Routes>
-    <Route path="snd" element={<><ResponsiveAppBar></ResponsiveAppBar><Home/></>}/>
+    <Route path="/" element={<Tracker/>}/>
     <Route path="about" element={<AboutUs/>}/>
     <Route path="contact" element={<Contact/>}/>
+    <Route path="/login" element={<FormEx/>}/>
     </Routes>
     </BrowserRouter>
     
--- src/form/FormEx.js
+++ src/form/FormEx.js
@@ -1,25 +1,46 @@
 import React, {useState } from 'react'
-
+import { useNavigate } from 'react-router-dom';
 function FormEx() {
     const [un,setUn]=useState("");
     const [pass,setPass]=useState("");
+    const [error,setError]=useState("false")
+    const navigate=useNavigate()
+    const check=(ins)=>
+    {
+        if(un==="admin" && pass==="admin")
+        {
+            
+            navigate("/")
+            console.log("Yes")
+            
+
+        }
+        }
+    
     const formHandler=(event)=>
     {
-        event.preventDefault();
+        if(un==="admin" && pass==="admin")
+        {     
         const loginObj={
             name:un,
             pwd:pass
         }
         console.log(loginObj)
-        alert(JSON.stringify(loginObj));
+        
+        }
+        else{
+            setError("true")
+    }
     }
   return (
     <>
 <h3>Login</h3>
 <form onSubmit={formHandler}>
     Username:<input type="text" value={un} placeholder='Username' onChange={(e)=>setUn(e.target.value)}></input><br/>
-    Password:<input type="password" value={pass} placeholder='password' onChange={(f)=>setPass(f.target.value)}></input><br/>
-    <button type='submit'>Login</button>
+    <div>{error && un.length===0?<label style={{color:'blue'}}>username required</label>:""}</div>
+Password:<input type="password" value={pass} placeholder='password' onChange={(f)=>setPass(f.target.value)}></input><br/>
+
+    <button type='submit' onClick={check}>Login</button>
 </form>
     </>
   )
